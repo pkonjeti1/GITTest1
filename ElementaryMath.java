8f9150d669aff6025adce4179ae4cb4f33915728
pkonjeti1 <33296456+pkonjeti1@users.noreply.github.com>
2017-11-01 15:29:46 -0500

    Add files via upload

---
+++ ElementaryMath.java
@@ -0,0 +1,24 @@
+package com.web.basicservice;
+
+public class ElementaryMath {
+	
+	public float addNumbers(float number1, float number2){
+		return number1 + number2;
+	}
+
+	public float subtractNumbers(float number1, float number2){
+		return number1 - number2;
+	}
+	
+	public float multiplyNumbers(float number1, float number2){
+		return number1 * number2;
+	}
+
+	public float divideNumbers(float number1, float number2){
+		return number1 / number2;
+	}
+	
+	/*public static void main(String args[]){
+		System.out.println("adding two numbers 4.5, 5.5" + addNumbers(4, 5));
+	}*/
+}
